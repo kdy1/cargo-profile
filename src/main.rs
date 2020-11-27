fcb18e3b5e8c16e13998076e82fc254adbd5deb1
강동윤 <kdy1997.dev@gmail.com>
2020-11-28 00:31:25 +0900

    cargo profile get-bin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::bail;
 use anyhow::Context;
 use anyhow::Error;
 use cargo::compile;
@@ -9,7 +10,7 @@ mod flamegraph;
 
 #[derive(StructOpt)]
 #[structopt(about = "The performance profiler for cargo")]
-pub enum Command {
+pub enum SubCommand {
     /// Run all benchmark and store result as a json file.
     All,
     /// Create a flamegraph for given target
@@ -25,19 +26,42 @@ pub enum Command {
         #[structopt(long)]
         release: bool,
     },
+
+    /// Compile a binary using cargo and print absolute path to the file.
+    ///
+    /// Usage: perf record `cargo profile get-bin bench --bench fixture`
+    GetBin {
+        /// Compile library
+        #[structopt(subcommand)]
+        target: CargoTarget,
+
+        #[structopt(long)]
+        release: bool,
+    },
 }
 
 fn main() -> Result<(), Error> {
-    let cmd: Command = Command::from_args();
+    let cmd: SubCommand = SubCommand::from_args();
 
     match cmd {
-        Command::All => {}
-        Command::Flamegraph {
+        SubCommand::All => {}
+        SubCommand::Flamegraph {
             root,
             target,
             release,
         } => {
             compile(release, &target).context("cargo execution failed")?;
+        }
+
+        SubCommand::GetBin { target, release } => {
+            let binraries = compile(release, &target).context("cargo execution failed")?;
+            if binraries.len() != 1 {
+                bail!(
+                    "cargo produced too many binaries, which is not supoprted by `cargo profile \
+                     bin-path`"
+                )
+            }
+            println!("{}", binraries[0].path.display());
         }
     }
 
---
+++ src/perf_report/mod.rs
@@ -0,0 +1,30 @@
+use crate::cargo::BinFile;
+use anyhow::Context;
+use anyhow::Error;
+use std::process::Command;
+use std::process::Stdio;
+
+pub fn profile(file: &BinFile) -> Result<(), Error> {
+    let mut cmd = Command::new("xcrun");
+
+    cmd.stdin(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .arg("xctrace")
+        .arg("record");
+
+    cmd.arg("--template").arg("Time Profiler");
+
+    cmd.arg("--launch")
+        .arg("--")
+        .arg(&file.path)
+        .spawn()
+        .with_context(|| {
+            format!(
+                "failed to spawn xcrun xctrace record `{}`",
+                file.path.display(),
+            )
+        })?;
+
+    Ok(())
+}
