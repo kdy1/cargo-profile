c2b1e189f813cf6e5c89cd1274af22a26d0ffcd2
강동윤 <kdy1997.dev@gmail.com>
2020-11-29 10:56:15 +0900

    Add cpu profiler for mac os (#5)

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use crate::cargo::compile;
 use crate::cargo::CargoTarget;
 use crate::cli_tools::dtrace::make_dtrace_command;
@@ -7,12 +5,19 @@ use crate::cli_tools::profiler::run_profiler;
 use anyhow::bail;
 use anyhow::Context;
 use anyhow::Error;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use structopt::StructOpt;
 use tempdir::TempDir;
+
+mod merge;
 
 /// WIP: Profiles cpu usage.
 #[derive(Debug, Clone, StructOpt)]
 pub enum CpuCommand {
+    /// Profiles the program and print results in order of (total, local, function name).
+    ///
+    /// Note that cpu usage can be larger thsn 100%, if threads are used.
     PerFn {
         /// Use sudo.
         #[structopt(long)]
@@ -63,8 +68,22 @@ impl CpuCommand {
 
                     let collapsed = String::from_utf8_lossy(&collapsed);
 
-                    process_collapsed(&collapsed)
+                    let (time, mut data) = process_collapsed(&collapsed)
                         .context("failed to process collapsed stack data")?;
+                    data.sort_by_key(|info| info.total_used);
+
+                    println!(
+                        "{: <10}  | {: <10}  | {}",
+                        "Totql time", "Own time", "File name",
+                    );
+                    for info in data.iter().rev() {
+                        println!(
+                            "{: <10.1}% | {: <10.1}% | {}",
+                            info.total_used as f64 / time as f64 * 100f64,
+                            info.self_used as f64 / time as f64 * 100f64,
+                            info.name,
+                        );
+                    }
                 }
 
                 Ok(())
@@ -73,38 +92,64 @@ impl CpuCommand {
     }
 }
 
-fn process_collapsed(data: &str) -> Result<(), Error> {
-    let mut fn_time_including_deps = HashMap::<_, usize>::new();
-    let mut fn_time_itself = HashMap::<_, usize>::new();
+struct FnTimingInfo {
+    name: String,
+    total_used: usize,
+    /// The percentage of time used by function code itself.
+    self_used: usize,
+}
 
-    for line in data.lines() {
-        let items = line.split(";");
-        let items_count = items.clone().count();
+fn process_collapsed(data: &str) -> Result<(usize, Vec<FnTimingInfo>), Error> {
+    let mut lines: Vec<&str> = data.lines().into_iter().collect();
+    lines.reverse();
+    let (frames, time, ignored) =
+        merge::frames(lines, true).context("failed to merge collapsed stack frame")?;
 
-        for (idx, mut item) in items.enumerate() {
-            let is_last = idx == items_count - 1;
-
-            // println!("Item: {}", item);
-
-            let splitted = item.split('`');
-            if splitted.clone().count() != 2 {
-                log::warn!("process_collapsed: ignoring wrong item (not separated by '`')");
-                continue;
-            }
-
-            if is_last {
-                item = item.trim_end_matches(|c: char| c.is_digit(10) || c == ' ');
-            }
-
-            *fn_time_including_deps.entry(item).or_default() += 1;
-            if is_last {
-                // TODO: This is wrong.
-                *fn_time_itself.entry(item).or_default() += 1;
-            }
-        }
+    if time == 0 {
+        bail!("No stack counts found")
     }
 
-    // println!("{:#?}", fn_time_including_deps);
+    if ignored > 0 {
+        eprintln!("ignored {} lines with invalid format", ignored)
+    }
 
-    Ok(())
+    let mut total_time = HashMap::<_, usize>::new();
+    let mut itself_time = HashMap::<_, usize>::new();
+
+    // Check if time collapses
+    for frame in &frames {
+        let fn_dur = frame.end_time - frame.start_time;
+        *total_time.entry(frame.location.function).or_default() += fn_dur;
+
+        let children = frames.iter().filter(|child| {
+            frame.location.depth + 1 == child.location.depth
+                && frame.start_time <= child.start_time
+                && child.end_time <= frame.end_time
+        });
+
+        let mut itself_dur = fn_dur;
+
+        for child in children {
+            let fn_dur = child.end_time - child.start_time;
+            itself_dur -= fn_dur;
+        }
+
+        *itself_time.entry(frame.location.function).or_default() += itself_dur;
+    }
+
+    let mut result = vec![];
+    let mut done = HashSet::new();
+
+    for frame in &frames {
+        if !done.insert(&frame.location.function) {
+            continue;
+        }
+        result.push(FnTimingInfo {
+            name: frame.location.function.to_string(),
+            total_used: *total_time.entry(frame.location.function).or_default(),
+            self_used: *itself_time.entry(frame.location.function).or_default(),
+        });
+    }
+
+    Ok((time, result))
 }
